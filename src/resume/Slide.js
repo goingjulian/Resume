8d9d0794dafa987281a04b6a1cec3d2140a584a2
Julian Korf de Gidts <julian@korfdegidts.nl>
2019-09-05 14:58:00 +0200

    Multiple improvements

--- src/resume/Slide.js
+++ src/resume/Slide.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import ListTable from './slideComponents/ListTable/ListTable';
+import GeneralInfo from './slideComponents/GeneralInfo/GeneralInfo';
 
 export default props => {
     return <article className={props.className} >
         <h1>{props.title}</h1>
-
+        
         <div className="content">
-            {props.img ? <img src={`assets/${props.img}`} alt="" className="slideImg" /> : null}
-            <p>{props.text}</p>
+            {props.listTable ? <ListTable lists={props.listTable}/> : <GeneralInfo img={props.img} text={props.text}/>}
         </div>
     </article>
 }
