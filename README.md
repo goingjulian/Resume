5471ea4106709a7cf548e19773c4f6fe1360a2d0
Julian Korf de Gidts <julian@korfdegidts.nl>
2019-09-09 22:54:15 +0200

    Cleanup gh-pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# My resume
-This is the source code for my online resume, made with React. You can view the live version on [julian.korfdegidts.nl](https://julian.korfdegidts.nl)
--- src/resume/NavButtons.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default props => {
-    return <footer className="navButtons">
-        <button onClick={() => props.scrollToSlide(props.currentSlide - 1)} disabled={props.currentSlide > 0 ? false : true}>Previous</button>
-        <button onClick={() => props.scrollToSlide(props.currentSlide + 1)} disabled={props.currentSlide < props.slidesArrLength - 1 ? false : true}>Next</button>
-    </footer>
-}
