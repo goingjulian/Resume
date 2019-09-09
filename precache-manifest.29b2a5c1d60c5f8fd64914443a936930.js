fc0ef371448152a9dd5e576d98730c30aaa8f53a
Julian Korf de Gidts <julian@korfdegidts.nl>
2019-09-09 22:55:43 +0200

    More cleanup

---
+++ precache-manifest.29b2a5c1d60c5f8fd64914443a936930.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "696d2752913138367874cd370e98dc0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15af4ac47802dd76a61a",
+    "url": "/static/css/main.a4459655.chunk.css"
+  },
+  {
+    "revision": "7fc30ce28cf17f1e9778",
+    "url": "/static/js/2.50f65320.chunk.js"
+  },
+  {
+    "revision": "15af4ac47802dd76a61a",
+    "url": "/static/js/main.b2ff67e1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/resume/slideComponents/GeneralInfo/GeneralInfo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import "./GeneralInfo.css";
-
-export default props => {
-    return <>
-        {props.img ? <img src={`assets/${props.img}`} alt="" className="slideImg" /> : null}
-        <p>{props.text}</p>
-    </>
-}
